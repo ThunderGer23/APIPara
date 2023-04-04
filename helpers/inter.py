0bc7959bb01f82afe1cfef886b76803811d0208c
ThunderGer <Jaames11@hotmail.com>
2023-04-03 22:41:32 -0600

    ok :v

--- helpers/inter.py
+++ helpers/inter.py
@@ -3,30 +3,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 from helpers.model import model
 
 def Interprete (data_base, data_com ):
-  frases=[]
-  frases.append(data_base)
-  for item in data_com :
-    frases.append(item)
-  print("frases a comparar:")
-  print(frases[1:])
-  print("\n")
+  frases= [data_base] + data_com
   frases_embeddings = model.encode(frases)
-  a=cosine_similarity(
-    [frases_embeddings[0]],
-    frases_embeddings[1:]
-    )
-  print("con la siguiente oracion:\n")
-  print(data_base)
-  print("\n\n oracion con mas equivalencia semantica (parafraseo): \n")
-  #print(len(a))
-  #print(type(a))
-  #print(a)
-  r_f = a[0]
-  #print(r_f)
-  #print(r_f)
-  count=0
-  for item in r_f:
-    count = count+1
-    if item >= 0.9:
-      return frases[count]
-  return ["Sin equivalencias semanticas"]
+  a= cosine_similarity( [frases_embeddings[0]], frases_embeddings[1:])[0]
+  for i in range(0, len(a)):
+    if a[i] >= 0.9 : return frases[i+1]
+  return "Sin equivalencias semanticas"
