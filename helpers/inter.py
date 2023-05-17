62a6231536b99a43d91a3b66da0330e347361517
ThunderGer <jaames11@hotmail.com>
2023-05-17 09:55:20 -0600

    Testeando para obtener un conjunto de frases

--- helpers/inter.py
+++ helpers/inter.py
@@ -7,24 +7,15 @@ def Interprete (data_base, data_com ):
   frases.append(data_base)
   for item in data_com :
     frases.append(item)
-  print("frases a comparar:")
-  print(frases[1:])
-  print("\n")
+  print("frases a compar
   frases_embeddings = model.encode(frases)
   a=cosine_similarity(
     [frases_embeddings[0]],
     frases_embeddings[1:]
     )
-  print("con la siguiente oracion:\n")
-  print(data_base)
-  print("\n\n oracion con mas equivalencia semantica (parafraseo): \n")
-  #print(len(a))
-  #print(type(a))
-  print(a)
-  r_f = a[0]
-  #print(r_f)
-  #print(r_f)
-  count=0
+  r_f = a[0].sort()
+  print(r_f)
+  count = 0
   for item in r_f:
     count = count+1
     if item >= 0.9:
